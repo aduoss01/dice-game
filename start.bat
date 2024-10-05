8e14abb67abae4158fe7f06053f157ace7e69ac2
aduoss01 <151424090+aduoss01@users.noreply.github.com>
2024-10-05 12:47:23 +0900

    feat: remove start batch file

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-C:/Users/tmax/AppData/Local/Programs/Python/Python312/python.exe -m black .
-C:/Users/tmax/AppData/Local/Programs/Python/Python312/python.exe -m isort .
-cls
-
-echo Starting server
-cd src/
-C:/Users/tmax/AppData/Local/Programs/Python/Python312/python.exe c:/Users/tmax/Downloads/skeleton_dicegame/src/DicegameServer.py
-PAUSE
